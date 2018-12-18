1ef589555c53121d8b110a7833067a6f2d0c32f1
cheshirecatnick <cheshirecatnick@gmail.com>
2018-12-18 11:26:34 +0800

    finish adaptive attacker

--- attacker/vmware-attacker/adaptive-attacker.js
+++ attacker/vmware-attacker/adaptive-attacker.js
@@ -7,12 +7,12 @@ class AdaptiveAttacker extends Attacker {
 
     attack(packets) {
         const msg = packets[0].content;
-        if (msg.type === 'fl-propose' && packets.length === 1) {
+        if (this.mode === 'vrf' && 
+            msg.type === 'fl-propose' && 
+            packets.length === 1) {
             this.propose.push(msg);
             if (this.propose.length == config.nodeNum) {
                 //console.log('find best vrf except byzantines');
-                this.propose = this.propose
-                    .filter(msg => !this.byzantines.has(msg.sender));
                 this.propose.sort((msgA, msgB) => {
                     if (msgA.kL < msgB.kL) {
                         return 1;
@@ -28,7 +28,7 @@ class AdaptiveAttacker extends Attacker {
                 console.log(bestProposal);
                 // adapt leader
                 if (this.byzantines.has(bestProposal.sender)) {
-                    console.log('lucky adapter');
+                    console.log('lucky VRF');
                 }
                 else if (this.byzantines.length < this.f) {
                     console.log('adapting: ', bestProposal.sender);
@@ -37,9 +37,9 @@ class AdaptiveAttacker extends Attacker {
                 // send fork value
                 if (this.byzantines.has(bestProposal.sender)) {
                     const forkProposeMsg = JSON.parse(JSON.stringify(bestProposal));
-                    forkProposeMsg.proposeMsg.vL = 'a forked value';
-                    // broadcast to some of the honest nodes
-                    for (let nodeID = 1; nodeID <= 5; nodeID++) {
+                    forkProposeMsg.proposeMsg.vL = 'x'.repeat(32);
+                    // broadcast to half of the honest nodes
+                    for (let nodeID = 1; nodeID <= (config.nodeNum / 2); nodeID++) {
                         packets.push({
                             src: bestProposal.sender,
                             dst: '' + nodeID,
@@ -50,6 +50,44 @@ class AdaptiveAttacker extends Attacker {
                 }
                 this.propose = [];
             }
+        }
+        else if (this.mode === 'adaptive' && 
+            msg.type === 'elect' && 
+            packets.length === 1) {
+            this.propose.push(msg);
+            if (this.propose.length == config.nodeNum) {
+                //console.log('find best vrf except byzantines');
+                this.propose.sort((msgA, msgB) => {
+                    return (msgA.y < msgB.y) ? 1 : -1;
+                });
+                const bestProposal = this.propose[0];
+                console.log(bestProposal);
+                // adapt leader
+                if (this.byzantines.has(bestProposal.sender)) {
+                    console.log('lucky VRF');
+                }
+                else if (this.byzantines.length < this.f) {
+                    console.log('adapting: ', bestProposal.sender);
+                    this.byzantines.push(bestProposal.sender);
+                }
+                // send fork value
+                if (this.byzantines.has(bestProposal.sender)) {
+                    /*
+                    const forkProposeMsg = JSON.parse(JSON.stringify(bestProposal));
+                    forkProposeMsg.proposeMsg.vL = 'x'.repeat(32);
+                    // broadcast to half of the honest nodes
+                    for (let nodeID = 1; nodeID <= (config.nodeNum / 2); nodeID++) {
+                        packets.push({
+                            src: bestProposal.sender,
+                            dst: '' + nodeID,
+                            content: forkProposeMsg,
+                            delay: 0
+                        });
+                    }*/
+                }
+                this.propose = [];
+            }
+            
         }
         return packets;
     }
